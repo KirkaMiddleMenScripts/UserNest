b2dc40f2ec77964ec85b5b25979e15322be4fbc7
KirkaMiddleMenScripts <Kirkamiddlemen@gmail.com>
2025-06-09 01:04:10 -0400

    Delete js directory

--- js/supabase.js
+++
@@ -1,3 +0,0 @@
-const supabaseUrl = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzdG1uYXpoZ2FmZWRyenFqa21uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0MzQ5MDgsImV4cCI6MjA2NTAxMDkwOH0.FA2fKqFxzwmrys0i3VG2WPnvNY9XhPISxMsy_S3uJHg';
-const supabaseKey = 'https://zstmnazhgafedrzqjkmn.supabase.co';
-window.supabase = supabase.createClient(supabaseUrl, supabaseKey);
